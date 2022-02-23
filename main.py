57c77db173d84111e229c76e02be00c15765ddca
fish5 <>
2022-02-23 01:38:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+print ("----welcome to SPLIT MY PIZZA----")
+
+print ("How many slices do you have?")
+
+Total_slices = int(input())
+
+print ("How many people are sharing?")
+
+Total_people = int(input())
+
+print ("How many slices for each person")
+
+Slices_perperson = Total_slices // Total_people
+
+print (f"{Slices_perperson}")
+
+Slices_left = Total_slices % Total_people
+
+print ("remaining slices")
+
+print (f"{Slices_left}")
+
+print ('''                                    ._
+                                   ,(  `-.
+                                 ,': `.   `.
+                               ,` *   `-.   \
+                             ,'  ` :+  = `.  `.
+                           ,~  (o):  .,   `.  `.
+                         ,'  ; :   ,(__) x;`.  ;
+                       ,'  :'  itz  ;  ; ; _,-'
+                     .'O ; = _' C ; ;'_,_ ;
+                   ,;  _;   ` : ;'_,-'   i'
+                 ,` `;(_)  0 ; ','       :
+               .';6     ; ' ,-'~
+             ,' Q  ,& ;',-.'
+           ,( :` ; _,-'~  ;
+         ,~.`c _','
+       .';^_,-' ~
+     ,'_;-''
+    ,,~
+    i'
+    :''')
+       
+       
+      
+
+
+
+
+
+     
